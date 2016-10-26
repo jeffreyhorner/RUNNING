02223860a9bb358b01040a733fbf0fafe4ee3bb7
jeffreyhorner <jeffrey.horner@gmail.com>
2016-10-26 15:33:38 -0500

    Garmin Ephemera

--- Garmin_Ephemera/GARMIN_STREAM_EPHEMERA.sh
+++ Garmin_Ephemera/GARMIN_STREAM_EPHEMERA.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -H "Garmin-Client-Name: CoreService" -H "Content-Type: application/octet-stream" --data-binary @garmin-postdata http://omt.garmin.com/Rce/ProtobufApi/EphemerisService/GetEphemerisData | ./strip_checksum.pl
+GARMIN_EPO_LOCATION=/media/hornerj/GARMIN/GARMIN/REMOTESW/EPO.BIN
+curl -H "Garmin-Client-Name: CoreService" -H "Content-Type: application/octet-stream" --data-binary @garmin-postdata http://omt.garmin.com/Rce/ProtobufApi/EphemerisService/GetEphemerisData | ./strip_checksum.pl > $GARMIN_EPO_LOCATION
